4ff32a33b1a3b6dc8fdb32adfc41ddc3c7af842b
avatar <avatar.android@gmail.com>
2010-04-29 22:38:15 -0700

    more makefile cleanup

--- device.mk
+++ device.mk
@@ -2,31 +2,9 @@
 $(call inherit-product, build/target/product/generic.mk)
 include vendor/htc/passion/device_passion.mk
 
-PRODUCT_PACKAGES += \
-    DeskClock \
-    DownloadProvider \
-    Gallery3D \
-    GlobalSearch \
-    Launcher2 \
-    LiveWallpapers \
-    LiveWallpapersPicker \
-    MagicSmokeWallpapers \
-    SoundRecorder \
-    VisualizationWallpers \
-    VoiceDialer \
-    libRS \
-    librs_jni \
-    lights.mahimahi \
-    sensors.mahimahi
-
-# How this product is called in the build system
 PRODUCT_NAME := passion
-
-# Which actual hardware this is based on (this is a path under vendor/)
 PRODUCT_BRAND := google
 PRODUCT_DEVICE := passion
-
-# The user-visible product name
 PRODUCT_MODEL := Nexus One
 PRODUCT_MANUFACTURER := HTC
 
@@ -34,7 +12,4 @@ PRODUCT_MANUFACTURER := HTC
 include frameworks/base/data/sounds/AudioPackage4.mk
 
 # This is the list of locales included in AOSP builds
-PRODUCT_LOCALES := en_US en_GB fr_FR it_IT de_DE es_ES
-
-# This is a high DPI device, so add the hdpi pseudo-locale
-PRODUCT_LOCALES += hdpi
+PRODUCT_LOCALES := en_US de_AT de_CH de_DE fr_FR hdpi
